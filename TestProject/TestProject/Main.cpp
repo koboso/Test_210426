4bfc6829fe74ba4be7cd9da04f2a8235238dfe88
home <HOME@HOME.COM>
2021-04-26 23:05:59 +0900

    good

--- TestProject/TestProject/Main.cpp
+++ TestProject/TestProject/Main.cpp
@@ -1,11 +1,12 @@
 #include"Main.h"
 #define S_SIZE 10//스택 사이즈 10은 매크로 처리
+int stack[S_SIZE];
+int top = 0;
+
 
 void main() {
 	//스택은 FirstInLastOut 방식
-	int top = 0;
 
-	int stack[S_SIZE];
 
 	for (int i = 0; i < S_SIZE; i++)
 	{
@@ -22,14 +23,14 @@ void main() {
 		if (top < 0) break;
 
 		else {
-			printf("%d", a);
+			printf("%d \n", a);
 		}
 	}
 }
 
 	//스택이 비었을 때 불러오면 "값이 비었습니다."를 출력하는 IsStackEmpty()함수
 	bool IsStackEmpty() {
-		if (stack[0] == NULL) {
+		if (top == -1) {
 			return true;
 		}
 		else return false;
@@ -44,12 +45,12 @@ void main() {
 //스택에 값을 저장할 Push()함수
 void Push(int a) {
 	if (IsStackFull() == true){
-		printf("스택이 가득 찼습니다.\n")
+		printf("스택이 가득 찼습니다.\n");
 	}
 	else
 	{
+		++top;
 		stack[top] = a;
-		top++;
 	}
 }
 //스택의 값을 불러 올 Pop()함수
