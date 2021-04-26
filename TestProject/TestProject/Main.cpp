d914ac427a877c40f309a6128c509b63c2606e0d
maxmgmg <joaunmg@knou.ac.kr>
2021-04-26 22:59:32 +0900

    두번쨰 푸쉬

--- TestProject/TestProject/Main.cpp
+++ TestProject/TestProject/Main.cpp
@@ -1,37 +1,47 @@
 #include "Main.h"
 
-int top = 0;
-void Push(int);
+int top = -1;
+int stack[STACK_SIZE];
+void Push(int a);
 int Pop();
 bool IsStackEmpty();
 bool IsStackFull();
 
 void main() {
-	Push(2);
-	Pop();
+	Push(3);
+	
+
+
 	fgetc(stdin);
 }
 
 void Push(int a) {
-	top = a;
+	if (IsStackFull()) {
+		printf("스택이 가득찼습니다. \n\n");
+		return;
+	}
+	else {
+		++top;
+		stack[top] = a;
+	}
 }
 
 int Pop() {
-	if (top!=NULL) {
-		printf("%d \n\n", top);
+	if (IsStackEmpty()) {
+		printf("값이 비었습니다. \n\n");
 	}
 	else {
-		IsStackEmpty();
+		printf("%d\n\n", top);
+		return 0;
 	}
-	return 0;
 }
 
 bool IsStackEmpty() {
-	printf("값이 비었습니다. \n\n");
-	return 0;
+	if (stack < 0) return true;
+	return false;
 }
 
 bool IsStackFull() {
-	printf("스택이 가득찼습니다. \n\n");
-	return 0;
+	if (stack >= STACK_SIZE)  return true;
+	return false;
 }
